6971af95765201d58267a242ba85be8db6b89246
xo81920-dot <xo81920@gmail.com>
2026-05-06 00:54:38 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HpPrinter implements Printer {
+    @Override
+    public void print(String message) {
+        System.out.println("HP印表機： " + message);
+    }
+}
