625e50b93b4a63ec94aa1016e1c211ecca28fb58
xo81920-dot <xo81920@gmail.com>
2026-05-07 00:12:57 +0800

    Update printer behavior

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MyController {
+public class  MyController {
 
     @Autowired
     private Printer printer;
