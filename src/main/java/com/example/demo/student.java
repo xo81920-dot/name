ecfb529c863c7732d295547e6d7498074ee46e3d
xo81920-dot <xo81920@gmail.com>
2026-06-01 00:17:32 +0800

    Add JSON binding demo

---
+++ src/main/java/com/example/demo/student.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import java.util.List;
+
+public class student {
+    Integer id;
+    String name;
+    Double store;
+    boolean graduated;
+    List<String> courseList;
+    Pet pet ;
+    List<Pet> petList;
+}
